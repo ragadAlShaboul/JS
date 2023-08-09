6181a692989787556b00a63159ec333186c3d688
RagadAlShaboul <ragad.alshaboul@gmail.com>
2023-08-09 12:18:00 +0300

    MemoryCardGame

--- MemoryCardGame/main.js
+++ MemoryCardGame/main.js
@@ -56,7 +56,9 @@ const hide = function () {
     const image = document.getElementById(`image ${i}`);
     image.src = "q.png";
   });
-}
+};
+let counter = 0;
+
 const HandleClicks = function () {
   let button = document.getElementById("00");
   let right = 0;
@@ -70,38 +72,46 @@ const HandleClicks = function () {
     images.push(document.getElementById(`image ${i}`));
     button.onclick = function () {
       clicks++;
-      trial.push(photos[i]);
-      ids0.push(i);
-      images[i].src = photos[i];
-      if (
-        clicks % 2 === 0 &&
-        trial[trial.length - 1] === trial[trial.length - 2]
-      ) {
-        right++;
-        failure = 0;
-      } else if (clicks % 2 === 0 && clicks !== 0) {
-        setTimeout(() => {
-          images[ids0[ids0.length - 1]].src = "q.png";
-          images[ids0[ids0.length - 2]].src = "q.png";
-        }, 500);
-        
-        failure++;
+      counter++;      
+      
+      if(counter <= 2){
+        ids0.push(i);
+        trial.push(photos[i]);
+        images[i].src = photos[i];
+        if (
+          clicks % 2 === 0 &&
+          trial[trial.length - 1] === trial[trial.length - 2]
+        ) {
+          right++;
+          failure = 0;
+          counter=0;
+        } else if (clicks % 2 === 0 && clicks !== 0) {
+          setTimeout(() => {
+            images[ids0[ids0.length - 1]].src = "q.png";
+            images[ids0[ids0.length - 2]].src = "q.png";
+            counter = 0;
+          }, 500);
+  
+          failure++;
+        }
+  
+        if (right === photos.length / 2) {
+          score += 100;
+          displayScore("YOU WON");
+          play();
+        }
+        if (failure === 3) {
+          score -= 100;
+          displayScore("YOU LOST");
+          play();
+        }
+
       }
-      if (right === photos.length / 2) {
-        score += 100;
-        displayScore("YOU WON");
-        play();
-      }
-      if (failure === 3) {
-        score -= 100;
-        displayScore("YOU LOST");
-        play();
-      }
+     
     };
   }
 };
 const play = function () {
-  
   game.innerHTML = "";
   shuffle(photos);
   console.log(photos);
@@ -129,4 +139,5 @@ const displayScore = function (str) {
   statusDiv.id = "statusDiv";
   localStorage.setItem("score", score);
 };
+displayScore("");
 play();
